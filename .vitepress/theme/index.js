bf148d75dd3a85d9913a68ca38e046bcad2101b3
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-09-08 18:15:23 +0200

    remove dot

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -9,16 +9,13 @@ import './custom.scss'
 import { createGtm } from '@gtm-support/vue-gtm'
 
 export default {
-
   enhanceApp({ app, router, siteData }) {
     // app is the Vue 3 app instance from `createApp()`.
     // router is VitePress' custom router. `siteData` is
     // a `ref` of current site-level metadata.
-
     // Check if we're in the client
-    
   },
-  mounted(){
+  mounted() {
     turnOnAnalytics()
     document.addEventListener('analyticsAccepted', () => {
       turnOnAnalytics()
@@ -27,7 +24,7 @@ export default {
     function turnOnAnalytics() {
       if (localStorage.getItem('analyticsAllowed') === 'yes') {
         console.log('using analytics')
-        this.$root..use(
+        this.$root.use(
           createGtm({
             id: 'GTM-TX33N7P', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
 
@@ -36,7 +33,7 @@ export default {
             // vueRouter: router, // Pass the router instance to automatically sync with router (optional)
           })
         )
-        }
+      }
     }
   },
   ...DefaultTheme,
