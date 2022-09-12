5b17f567596987500df06836e889e3d1e0115b63
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-09-12 14:36:03 +0200

    Added logo

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -34,18 +34,58 @@ export default defineConfig({
         href: "https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Noto+Sans+Mono:wght@700&display=swap",
       },
     ],
+    // Favicon
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "128x128",
+        href: "/icons/favicon-128x128.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/icons/favicon-96x96.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/icons/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/icons/favicon-16x16.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/ico",
+        href: "/favicon.ico",
+      },
+    ],
   ],
 
   themeConfig: {
     logo: "/logo.svg",
     nav,
     sidebar,
-
     footer: {
-      license: {
-        text: "MIT License",
-        link: "https://opensource.org/licenses/MIT",
-      },
       copyright: `Activist Handbook | Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International`,
     },
   },
