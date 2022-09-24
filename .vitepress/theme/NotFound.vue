9e8bfd481e0e9155c5b6d8ddea68b54578ddccd6
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-09-24 10:45:35 +0200

    Hydration fix

--- .vitepress/theme/NotFound.vue
+++ .vitepress/theme/NotFound.vue
@@ -1,42 +1,43 @@
 <template>
-  <div class="vp-doc" v-if="!router.route.path.startsWith('/en/')">
-    <h1>Oops, page not found 😕</h1>
-    <p>This page is empty, just like Shell's climate promises.</p>
-    <p></p>
-    <div>
-      <a href="/">Go home</a>
-      <a :href="'https://web.archive.org/web/*/' + currentURL" target="_blank"
-        >Archived version</a
-      >
+  <ClientOnly>
+    <div class="vp-doc" v-if="!router.route.path.startsWith('/en/')">
+      <h1>Oops, page not found 😕</h1>
+      <p>This page is empty, just like Shell's climate promises.</p>
+      <p></p>
+      <div>
+        <a href="/">Go home</a>
+        <a :href="'https://web.archive.org/web/*/' + currentURL" target="_blank"
+          >Archived version</a
+        >
+      </div>
+      <p style="margin-top: 64px">
+        <i>
+          Activist Handbook is written by volunteers, and sometimes something
+          goes wrong. You can help us fix this broken page! Everyone can edit
+          our website. Create a new page for this path or edit the broken link.
+        </i>
+      </p>
+      <strong>
+        <a href="https://edit.activisthandbook.org/new" target="_blank"
+          >Create article</a
+        >
+      </strong>
     </div>
-    <p style="margin-top: 64px">
-      <i>
-        Activist Handbook is written by volunteers, and sometimes something goes
-        wrong. You can help us fix this broken page! Everyone can edit our
-        website. Create a new page for this path or edit the broken link.
-      </i>
-    </p>
-    <strong>
-      <a href="https://edit.activisthandbook.org/new" target="_blank"
-        >Create article</a
-      >
-    </strong>
-  </div>
+  </ClientOnly>
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRouter } from "vitepress";
 const router = useRouter();
 
-let currentURL = ref("")
+let currentURL = ref("");
 
 onMounted(() => {
   currentURL.value = window.location.href;
+  if (router.route.path.startsWith("/en/")) {
+    router.go(router.route.path.replace("/en/", "/"));
+  }
 });
-
-if (router.route.path.startsWith("/en/")) {
-  router.go(router.route.path.replace("/en/", "/"));
-}
 </script>
 <style lang="scss" scoped>
 .vp-doc {
