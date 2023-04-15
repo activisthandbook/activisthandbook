985d970038e686005f932587c952f42c5793d948
Joppe Hoekstra <joppehoekstra@icloud.com>
2023-04-15 10:56:47 +0200

    Update ActionSmartSmall.vue

--- .vitepress/theme/components/actions/ActionSmartSmall.vue
+++ .vitepress/theme/components/actions/ActionSmartSmall.vue
@@ -2,7 +2,10 @@
   <div class="call-to-action primary-action small" tabindex="0" id="action-smart-small">
     <h1>⚠️ Urgent request: Please don't scroll away</h1>
     <p class="description">
-      We ask you, humbly, to help. Activist Handbook trains 6000 new activists every month. But very few people decide to donate. We depend on donations to keep our platform free and independent.
+      We ask you, humbly, to help. We depend on donations to stay free and independent. But very few people decide to donate.
+    </p>
+    <p class="description">
+      Activist Handbook trains 6000 new activists every month. We are a nonprofit and we have published over 450+ guides for change-makers.
     </p>
     <p class="description">
       We appreciate any donation, no matter how small. Give whatever you can afford this month:
